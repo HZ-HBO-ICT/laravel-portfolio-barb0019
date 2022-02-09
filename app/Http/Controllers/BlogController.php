416d227aed95eeae83d4169e4ad1ca6ba8a173a3
barb0019 <barb0019@hz.nl>
2022-02-09 15:08:36 +0100

    revert commit
    revert commit and adding the homework.

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\Http\Controllers;
+
+class BlogController
+{
+    public function show()
+    {
+        return view('Blog');;
+
+    }
+
+}
---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\Http\Controllers;
+
+class WelcomeController
+{
+    public function show()
+    {
+        return view('welcome');;
+
+    }
+
+}
